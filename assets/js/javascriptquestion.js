c570a1fb5361f320f43901601d243a3ccec180fc
kailas-kokate-innoplexus <kailas.kokate@innoplexus.com>
2016-08-21 13:22:53 +0530

    create javascript question project

---
+++ assets/js/javascriptquestion.js
@@ -0,0 +1,36 @@
+javascriptquestionapp.controller("javascriptquestionCtrl", function($scope) {
+	$scope.javascriptquestions = [{
+		"question" : "What is javascript?",
+		"answer" : "Javascript is an interpreted programming or script language used for webpage behaviour.",
+	}, {
+		"question" : "Who is the father of javascript?",
+		"answer" : "Brendan Eich.",
+	}, {
+		"question" : "First appeared of javascript?",
+		"answer" : "May 23, 1995.",
+	}, {
+		"question" : "Filename extension of javascript?",
+		"answer" : "Filename extension is filename.js.",
+	}, {
+		"question" : "What is the correct media type for JavaScript source code?",
+		"answer" : "application/javascript.",
+	}, {
+		"question" : "Type of format of javascript?",
+		"answer" : "Scripting language.",
+	}, {
+		"question" : "Difference between javascript and jquery?",
+		"answer" : "jQuery is a wrapper(library) of JavaScript. Javascript is a interpreted programming or script language whereas jQuery is a framework or library to help make writing in javascript much easier.",
+	}, {
+		"question" : "Difference between undefined and null in javascript?",
+		"answer" : "jQuery is a wrapper(library) of JavaScript. Javascript is a interpreted programming or script language whereas jQuery is a framework or library to help make writing in javascript much easier.",
+	}, {
+		"question" : "Example of undefined in javascript?",
+		"answer" : "var _myVar; <br />  console.log(_myVar); <small><small>//shows undefined</small></small> <br />  console.log(typeof _myVar); <small><small>//shows undefined</small></small>.",
+	}, {
+		"question" : "Example of null in javascript?",
+		"answer" : "var _myVar = null; <br />  console.log(_myVar); <small><small>//shows null</small></small> <br />  console.log(typeof _myVar); <small><small>//shows object</small></small>.",
+	}, {
+		"question" : "Difference between break and return in javascript?",
+		"answer" : "break exits a <b>loop</b>, <br /> return exits a <b>function</b>.",
+	}];
+}); 
